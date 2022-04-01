1af9263894d9278c873fc194034d7eb5aaf6c54c
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2022-04-01 19:55:17 -0300

    Container component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ButtonBalancefy from './components/Button';
-
+import Container from './components/Container';
 function App() {
   return (
     <div className="App">
+      <Container height="620px" width="380px"></Container>
       <ButtonBalancefy variant="secondary" width="550px">HAHAHAHAHAHAHA</ButtonBalancefy>
     </div>
   );
---
+++ src/components/Container/index.js
@@ -0,0 +1,23 @@
+function Container(props) {
+  return (
+    <>
+    <div style={{
+      background: "#131515",
+      height: props.height,
+      width: props.width,
+      left: "1509px",
+      top: "36px",
+      borderRadius: "10px"
+    }}>
+      {props.children}
+    </div>
+    </>
+  )
+}
+
+
+export default Container;
+
+/*      height: "620px",
+width: "380px",
+*/
