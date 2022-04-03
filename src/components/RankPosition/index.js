e2c8c6c2149782ef11737dd8bfcb9c8ffe8a2b22
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2022-04-03 16:50:55 -0300

    Create rank component

---
+++ src/components/RankPosition/index.js
@@ -0,0 +1,45 @@
+export default function RankPosition(props) {
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        width: "100%",
+        height: "51.2px",
+        background: props.position % 2 === 0 ? "#131515" : "#2B2C28",
+        alignItems: "center",
+      }}>
+        <div style={{
+          display: "flex",
+          width: "44px",
+          height: "37px",
+          background: "#7DE2D1",
+          borderRadius: "5px",
+          color: "#000",
+          alignItems: "center",
+          fontSize: "18px",
+          marginLeft: "20px",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          #{props.position}
+        </div>
+        <div style={{
+          marginLeft: "15px",
+          width: "200px"
+        }}>
+          {props.name}
+        </div>
+        <div style={{
+        }}>
+          {props.goals}
+        </div>
+        <div style={{
+          marginLeft: "50px"
+        }}>
+          {props.tasks}
+        </div>
+
+      </div>
+    </>
+  );
+}
---
+++ src/components/Ranking/index.js
@@ -0,0 +1,40 @@
+import Container from "../Container";
+import RankPosition from "../RankPosition";
+
+export default function Ranking(props) {
+  const users = props.data
+
+  const Positions =
+    users.map((user) => {
+      return (
+        <RankPosition key={user.id} position={user.p} name={user.name} goals={user.goals} tasks={user.tasks}></RankPosition>
+      );
+    })
+
+  return (
+    <>
+      <Container height="372px" width="380px">
+        <h3 style={{textAlign: "center", marginTop: "5px"}}>
+          Ranking
+        </h3>
+          {Positions}
+      </Container>
+    </>
+  );
+}
+
+// Importe Ranking  
+
+// Antes do return coloque esse vetor:
+// const users = [
+//   { id: 1, p: 1, name: "Bruno Ferreira", goals: 1, tasks:13}, 
+//   { id: 2, p: 2, name: "Amanda", goals: 1, tasks:13 }, 
+//   { id: 3, p: 3,  name: "Julia Mendes", goals: 1, tasks:13 },
+//   { id: 4, p: 4,  name: "Lucas Alves", goals: 1, tasks:13 },
+//   { id: 5, p: 5,  name: "Beatriz Santos", goals: 1, tasks:1 },
+// ]
+
+// chame no App.js dentro do return() 
+// <Ranking data={users}></Ranking>
+
+// AINDA PRECISA ADICIONAR O TITULO NO TOPO!
