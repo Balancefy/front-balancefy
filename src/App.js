9135a0b53e017de1766504b747e34fb6b713f71b
Wesley Paternezi <wesley.paternezi@bandtec.com.br>
2022-04-09 20:58:04 -0300

    checkpoint

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import MaioresGastos from './components/MaioresGastos'
 function App() {
 
   const gastos = [
-    {key:3, id: 3, desc: "Comida", porcentagem: 17, color: "#339989"},
-    {key:1, id: 1, desc: "UBER", porcentagem: 55, color: "#7DE2D1"},
-    {key:2, id: 2, desc: "Roupa", porcentagem: 37, color: "#5DCAB8"},
+    {key:3, id: 3, desc: "Comida", porcentagem: 10, color: "#339989"},
+    {key:1, id: 1, desc: "UBER", porcentagem: 80, color: "#7DE2D1"},
+    {key:2, id: 2, desc: "Roupa", porcentagem: 10, color: "#5DCAB8"},
   ]
   
   return (
