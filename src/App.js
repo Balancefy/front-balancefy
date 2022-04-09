9d310a4e40a3243846831be46810c9c2cc8c9baa
AlmeidaEllen <ellen.almeida@bandtec.com.br>
2022-04-08 21:37:58 -0300

    componente comentário

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
+
 function App() {
   return (
     <>
+    
     </>
   );
 }
--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,7 +3,7 @@ function Container(props) {
   return (
     <>
     <div style={{
-      background: props.backgroundColor === "#4B4B4B" ? "#4B4B4B" : "131515",
+      background: props.backgroundColor === "#4B4B4B" ? "#4B4B4B" : "#2B2C28",
       height: props.height,
       width: props.width,
       borderRadius: props.borderRadius
