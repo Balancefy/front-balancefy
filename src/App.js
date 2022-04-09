38ead2de8333264e4dcb260e5ea1fd02b70b4e4d
Manocchio <guruu.leste@gmail.com>
2022-04-09 18:19:09 -0300

    create initial routes config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Menu from './components/Menu'
+import {Routes} from './routes'
 
 function App() {
-  return (
-    <>
-    </>
-  );
+  return <Routes/>
 }
 
 export default App; 
---
+++ src/routes/app.inside.routes.js
@@ -0,0 +1,17 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import Home from "../pages/Home";
+
+export const AuthenticatedRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </ Router>
+    )
+}
