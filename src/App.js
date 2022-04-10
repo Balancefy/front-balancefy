69995b92201758caba8ab1774e49ec5469b1c9b6
maats <maatsfa@gmail.com>
2022-04-10 17:39:34 -0300

    resolving conflits from App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Menu from './components/Menu'
-import Institucional from './pages/Institucional';
 import {Routes} from './routes'
 
 function App() {
-  return <Routes/>,
-  <Institucional></Institucional>
+  return <Routes/>
 }
 
 export default App; 
