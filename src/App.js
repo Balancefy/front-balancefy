df8c1642da8b12aa3a6ca08ec5c6c3a3c549784e
maats <maatsfa@gmail.com>
2022-04-10 17:36:17 -0300

    Starting Institutional page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Menu from './components/Menu'
+import Institucional from './pages/Institucional';
 import {Routes} from './routes'
 
 function App() {
-  return <Routes/>
+  return <Routes/>,
+  <Institucional></Institucional>
 }
 
 export default App; 
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,11 +3,11 @@ import { Button } from "@mui/material";
 function ButtonBalancefy(props) {
     const buttonTypes = {
         Primary: "primary",
-        Secondary: "secondary"
+        Secondary: "secondary",
     }
     return (
     <>
-        <Button type="submit" variant="contained" color={props.color}
+        <Button type="submit" variant={props.variant} color={props.color}
           style={{
             width: props.width,
             height: props.height,
