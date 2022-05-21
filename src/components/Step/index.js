9e030392d7162854afe6e96fd987193f3b6371f8
manocchio <guruu.leste@gmail.com>
2022-05-21 00:31:10 -0300

    steps component

--- src/components/Step/index.js
+++ src/components/Step/index.js
@@ -1,7 +1,8 @@
 import Container from "../Container";
-import StepVerde from "../../img/step-verde-escuro.svg";
-import StepVerdeAgua from "../../img/step-verde-agua.svg";
-import { red } from "@mui/material/colors";
+import StepVerde from "../../img/step-1.png";
+import StepVerdeAgua from "../../img/step-2.png";
+import StepBranco from "../../img/step-3.png";
+
 
 export default function Step(props) {
   return (
@@ -9,20 +10,33 @@ export default function Step(props) {
       <div
         style={{
           height: "100px",
-          width: "300px",
+          width: "400px",
           display: "flex",
           position: "relative",
           justifyContent: "space-between",
-          background: "red", 
-          marginTop: "10%",
         }}
       >
-        <div style={{ position: "absolute" }}>
-          <img src={StepVerde} />
+        <div style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", zIndex: 2 }}>
+          <div style={{ position: "relative", width: "fit-content", height: "fit-content" }}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "20px", color: "#2B2C28", fontWeight: "bold" }}>Passo 1</div>
+            <img height={65} width={160} src={StepVerde} />
+          </div>
         </div>
-        <div style={{ position: "absolute", left: "30" }}>
-          <img src={StepVerdeAgua} />
+
+        <div style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", left: "130px", zIndex: 1 }}>
+          <div style={{ position: "relative" }}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "20px", color: "#2B2C28", fontWeight: "bold" }}>Passo 2</div>
+            <img height={65} width={160} src={StepVerdeAgua} />
+          </div>
         </div>
+
+        <div style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", left: "260px", zIndex: 0 }}>
+          <div style={{ position: "relative" }}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "20px", color: "#2B2C28", fontWeight: "bold" }}>Passo 3</div>
+            <img height={65} width={160} src={StepBranco} />
+          </div>
+        </div>
+
       </div>
     </>
   );
